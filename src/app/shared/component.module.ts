79c767d4abe2644c89090fcaabbe07896db3aa45
Michael Hladky <michael@hladky.at>
2020-01-21 09:38:30 +0100

    added state

---
+++ src/app/shared/component.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+
+const DECLARATIONS = [];
+
+const EXPORTS = [DECLARATIONS];
+
+@NgModule({
+  declarations: [DECLARATIONS],
+  imports: [],
+  exports: [EXPORTS],
+})
+export class NgRxComponentModule {}
